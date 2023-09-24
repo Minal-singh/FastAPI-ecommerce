9ddcf8f9d428c23889f85a85171b4413be22cae1
Minal-singh <rishabhthkur15102@gmail.com>
2023-09-24 12:40:55 +0530

    Updated root route

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app.include_router(order_router, prefix="/order", tags=["order"])
 app.include_router(product_router, prefix="/product", tags=["product"])
 
 
-@app.get("/ping")
-async def ping():
-    return "pong"
+@app.get("/")
+async def root():
+    return {"message": "head to https://fastapi-ecommerce.onrender.com/docs for the swagger ui"}
